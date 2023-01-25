2136bd524c16b0b6942bb825f77c1be4f4e5bcc1
regorxxx <regorxxx@protonmail.com>
2023-01-25 16:44:52 +0100

    Paint on init.

--- seekbar.js
+++ seekbar.js
@@ -1,5 +1,5 @@
 'use strict';
-//24/01/23
+//25/01/23
 include('main\\seekbar\\seekbar_xxx_helper.js');
 include('main\\seekbar\\seekbar_xxx.js');
 include('helpers\\callbacks_xxx.js'); // Not needed if event listeners below are implemented as callbacks
@@ -11,9 +11,9 @@ const seekbar = new _seekbar({
 	ffprobe: arch === 'x64' // Should be set by user to not hard-code paths
 		? fb.ProfilePath + 'scripts\\SMP\\xxx-scripts\\helpers-external\\ffprobe\\bin\\win32\\x64\\ffprobe.exe'
 		: fb.ProfilePath + 'scripts\\SMP\\xxx-scripts\\helpers-external\\ffprobe\\bin\\win32\\ia32\\ffprobe.exe',
-	waveMode: 'bars',
+	waveMode: 'waveform',
 	paintMode: 'full',
-	analysisMode: 'RMS_level',
+	analysisMode: 'Peak_level',
 	x: 0,
 	w: window.Width,
 	scaleH: 0.9
@@ -23,8 +23,6 @@ const seekbar = new _seekbar({
 addEventListener('on_playback_new_track', (handle) => {
 	seekbar.newTrack(handle);
 });
-
-if (fb.IsPlaying) {on_playback_new_track(fb.GetNowPlaying()); seekbar.updateTime(fb.PlaybackTime);}
 
 addEventListener('on_playback_time', (time) => {
 	seekbar.updateTime(time);
@@ -53,3 +51,5 @@ addEventListener('on_mouse_rbtn_up', (x, y, mask) => {
 	seekbar.rbtn_up(x, y, mask);
 	return true; // left shift + left windows key will bypass this callback and will open default context menu.
 });
+
+if (fb.IsPlaying) {window.Repaint(); setTimeout(() => {on_playback_new_track(fb.GetNowPlaying()); seekbar.updateTime(fb.PlaybackTime);}, 0);}
