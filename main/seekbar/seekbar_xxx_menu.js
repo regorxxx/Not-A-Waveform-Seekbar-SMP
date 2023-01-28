d6120e69ef02809ad031fa140d63f0dc85e75f90
regorxxx <regorxxx@protonmail.com>
2023-01-28 10:36:26 +0100

    New options on menu.

--- main/seekbar/seekbar_xxx_menu.js
+++ main/seekbar/seekbar_xxx_menu.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-//25/01/23
+//275/01/23
 
 // Don't load this helper unless menu framework is also present
 // https://github.com/regorxxx/Menu-Framework-SMP
@@ -19,61 +19,73 @@ function createSeekbarMenu(bClear = true) {
 	menu.newEntry({entryText: 'sep'});
 	// Menus
 	{
-		const subMenu = menu.newMenu('Analysis type...');
-		const options = ['RMS_level', 'Peak_level', 'RMS_peak'];
+		const subMenu = menu.newMenu('Binary type...');
+		const options = ['ffprobe', 'audiowaveform'];
 		options.forEach((s) => {
 			menu.newEntry({menuName: subMenu, entryText: s, func: () => {
-				this.analysisMode = s;
+				this.analysis.binaryMode = s;
 				this.newTrack();
 				window.Repaint();
 			}});
 		});
-		menu.newCheckMenu(subMenu, options[0], options[options.length - 1], () => {return options.indexOf(this.analysisMode);});
+		menu.newCheckMenu(subMenu, options[0], options[options.length - 1], () => {return options.indexOf(this.analysis.binaryMode);});
+	}
+	{
+		const subMenu = menu.newMenu('Analysis type...');
+		const options = ['RMS_level', 'Peak_level', 'RMS_peak'];
+		options.forEach((s) => {
+			menu.newEntry({menuName: subMenu, entryText: s, func: () => {
+				this.analysis.analysisMode = s;
+				this.newTrack();
+				window.Repaint();
+			}});
+		});
+		menu.newCheckMenu(subMenu, options[0], options[options.length - 1], () => {return options.indexOf(this.analysis.analysisMode);});
 		menu.newEntry({menuName: subMenu, entryText: 'sep'});
 		{
 			['bNormalize']
 				.forEach((s) => {
 					menu.newEntry({menuName: subMenu, entryText: s, func: () => {
-						this[s] = !this[s];
+						this.analysis[s] = !this.analysis[s];
 						this.newTrack();
 						window.Repaint();
 					}});
-					menu.newCheckMenu(subMenu, s, void(0), () => {return this[s];});
+					menu.newCheckMenu(subMenu, s, void(0), () => {return this.analysis[s];});
 				});
 		}
 	}
 	menu.newEntry({entryText: 'sep'});
 	{
 		const subMenu = menu.newMenu('Waveform type...');
-		const options = ['waveform', 'bars'];
+		const options = ['waveform', 'bars', 'points','halfbars'];
 		options.forEach((s) => {
 			menu.newEntry({menuName: subMenu, entryText: s, func: () => {
-				this.waveMode = s;
+				this.preset.waveMode = s;
 				window.Repaint();
 			}});
 		});
-		menu.newCheckMenu(subMenu, options[0], options[options.length - 1], () => {return options.indexOf(this.waveMode);});
+		menu.newCheckMenu(subMenu, options[0], options[options.length - 1], () => {return options.indexOf(this.preset.waveMode);});
 	}
 	{
 		const subMenu = menu.newMenu('Paint mode...');
 		const options = ['full', 'partial'];
 		options.forEach((s) => {
 			menu.newEntry({menuName: subMenu, entryText: s, func: () => {
-				this.paintMode = s;
+				this.vpaintMode = s;
 				window.Repaint();
 			}});
 		});
-		menu.newCheckMenu(subMenu, options[0], options[options.length - 1], () => {return options.indexOf(this.paintMode);});
+		menu.newCheckMenu(subMenu, options[0], options[options.length - 1], () => {return options.indexOf(this.preset.paintMode);});
 	}
 	menu.newEntry({entryText: 'sep'});
 	{
 		['bPaintCurrent', 'bPaintFuture']
 			.forEach((s) => {
 				menu.newEntry({entryText: s, func: () => {
-					this[s] = !this[s];
+					this.preset[s] = !this.preset[s];
 					window.Repaint();
-				}, flags: this.waveMode !== 'waveform' && s === 'bPaintFuture' ? MF_GRAYED : MF_STRING});
-				menu.newCheckMenu(void(0), s, void(0), () => {return this[s];});
+				}, flags: this.paintMode === 'full' && s === 'bPaintFuture' ? MF_GRAYED : MF_STRING});
+				menu.newCheckMenu(void(0), s, void(0), () => {return this.preset[s];});
 			});
 	}
 	return menu;
