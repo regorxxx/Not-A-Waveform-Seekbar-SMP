0c8caa8b7f981a61992fc45f935d05716f443d79
regorxxx <regorxxx@protonmail.com>
2023-02-01 17:36:10 +0100

    Color themes on menu.

--- main/seekbar/seekbar_xxx_menu.js
+++ main/seekbar/seekbar_xxx_menu.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-//30/01/23
+//01/02/23
 
 // Don't load this helper unless menu framework is also present
 // https://github.com/regorxxx/Menu-Framework-SMP
@@ -78,6 +78,33 @@ function createSeekbarMenu(bClear = true) {
 				}, flags: (this.preset.paintMode === 'full' || this.analysis.binaryMode === 'visualizer') && s === 'bPaintFuture' ? MF_GRAYED : MF_STRING});
 				menu.newCheckMenu(void(0), s, void(0), () => {return this.preset[s];});
 			});
+	}
+	menu.newEntry({entryText: 'sep'});
+	{
+		const subMenu = menu.newMenu('Color presets...');
+		const options = [
+			{name: 'Green', colors: {main: colours.LimeGreen, alt: colours.LawnGreen, mainFuture: 0xFFB7FFA2, altFuture: 0xFFF9FF99, currPos: colours.White}},
+			{name: 'Lavender', colors: {main: 0xFFCDB4DB, alt: 0xFFFFC8DD, mainFuture: 0xFFBDE0FE, altFuture: 0xFFA2D2FF, currPos: 0xFFFFAFCC}},
+			{name: 'Forest', colors: {main: 0xFF606C38, alt: 0xFFDDA15E, mainFuture: 0xFF283618, altFuture: 0xFFBC6C25, currPos: 0xFF606C38}},
+			{name: 'Sienna', colors: {main: 0xFFBF0603, alt: 0xFFEE6055, mainFuture: 0xFF8D0801, altFuture: 0xFFC52233, currPos: 0xFF450920}},
+			{name: 'Blue', colors: {main: 0xFF003559, alt: 0xFF006DAA,mainFuture: 0xFF0353A4, altFuture: 0xFF061A40, currPos: 0xFFB9D6F2}},
+			{name: 'sep'},
+			{name: 'White Bg', colors: {bg: colours.White, bgFuture: colours.White}},
+			{name: 'White Bg (alt)', colors: {bg: colours.White, bgFuture: 0xFFF8F7FF}},
+			{name: 'Black Bg', colors: {bg: colours.Black, bgFuture: colours.Black}},
+			{name: 'Black Bg (alt)', colors: {bg: colours.Black, bgFuture: 0xFF1B1B1B}},
+			{name: 'Sienna Bg', colors: {bg: 0xFF450920, bgFuture: 0xFF74121D}},
+			{name: 'Blue Bg', colors: {bg: 0xFFB9D6F2, bgFuture: 0xFFBBDEFB}}
+		];
+		options.forEach((opt) => {
+			if (opt.name === 'sep') {
+				menu.newEntry({menuName: subMenu, entryText: opt.name});
+			} else {
+				menu.newEntry({menuName: subMenu, entryText: opt.name, func: () => {
+					this.updateConfig({ui: {colors: opt.colors}});
+				}});
+			}
+		});
 	}
 	return menu;
 }
