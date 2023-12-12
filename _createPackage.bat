759230d1251f547596c38d843927a4a0be784dd6
regorxxx <regorxxx@protonmail.com>
2023-12-12 21:20:04 +0100

    Update package builder

--- _createPackage.bat
+++ _createPackage.bat
@@ -1,6 +1,6 @@
 @ECHO off
 REM ------------------------------------------------------------------
-REM Create packages (zip file) from js files v.11/12/23
+REM Create packages (zip file) from js files v.12/12/23
 REM Requires 7za.exe on windows to compress (otherwise do it manually)
 REM If it's not provided, can be downloaded from:
 REM 	https://www.7-zip.org/download.html
@@ -113,7 +113,6 @@ CALL :copy_file helpers\helpers_xxx_UI_chars.js
 CALL :copy_file helpers\helpers_xxx_UI_flip.js
 CALL :copy_file helpers\helpers_xxx_web.js
 CALL :copy_file helpers\helpers_xxx_web_update.js
-CALL :copy_file helpers\map_xxx.js
 CALL :copy_file helpers\menu_xxx.js
 CALL :copy_file helpers\popup_xxx.js
 CALL :check_folder helpers\readme
@@ -332,12 +331,17 @@ CALL :check_root
 CALL :copy_main seekbar.js
 REM main
 CALL :copy_folder main\seekbar
+CALL :check_folder main\window
+CALL :copy_file main\window\window_xxx_background.js
+CALL :copy_file main\window\window_xxx_background_menu.js
+CALL :copy_file main\window\window_xxx_helpers.js
 REM helpers
 CALL :check_folder helpers
 CALL :copy_file helpers\callbacks_xxx.js
 CALL :copy_file helpers\helpers_xxx.js
 CALL :copy_file helpers\helpers_xxx_basic_js.js
 CALL :copy_file helpers\helpers_xxx_console.js
+CALL :copy_file helpers\helpers_xxx_dummy.js
 CALL :copy_file helpers\helpers_xxx_file.js
 CALL :copy_file helpers\helpers_xxx_foobar.js
 CALL :copy_file helpers\helpers_xxx_global.js
